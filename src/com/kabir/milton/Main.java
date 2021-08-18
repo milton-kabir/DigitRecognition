ee179de79e84bd5440ba2f4f60ffe0116adfe9b9
milton-kabir <milton.cse.pust@gmail.com>
2021-08-19 05:51:51 +0600

    First Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,8 +1,32 @@
+//package recognition;
 package com.kabir.milton;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        System.out.println("Input grid:");
+        String[] ar = new String[3];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            ar[i] = sc.nextLine();
+        }
+        int s = 0;
+        int[] br = {2, 1, 2, 4, -4, 4, 2, -1, 2, -5};
+        for (int i = 0, k = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++, k++) {
+                if (ar[i].charAt(j) == 'X') {
+                    s += br[k];
+                }
+            }
+        }
+        s += br[9];
+        if (s > 0) {
+            System.out.println("This number is 0");
+        } else {
+            System.out.println("This number is 1");
+        }
     }
 }
