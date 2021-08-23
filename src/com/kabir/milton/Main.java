6db7774cde69efbb9afbfcdfba53de9f6755a2d2
milton-kabir <milton.cse.pust@gmail.com>
2021-08-23 06:02:45 +0600

    Second Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -3,30 +3,58 @@ package com.kabir.milton;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main{
+    static Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Input grid:");
-        String[] ar = new String[3];
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 3; i++) {
-            ar[i] = sc.nextLine();
-        }
-        int s = 0;
-        int[] br = {2, 1, 2, 4, -4, 4, 2, -1, 2, -5};
-        for (int i = 0, k = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++, k++) {
-                if (ar[i].charAt(j) == 'X') {
-                    s += br[k];
-                }
+    public static int[] getNeuron() {
+        int[] neuron = new int[15];
+
+        for (int i = 0, k = 0; i < 5; i++) {
+            String temp = sc.next();
+            System.out.println(temp);
+            for (int j = 0; j < 3; j++) {
+                neuron[k++] = temp.charAt(j) == 'X' ? 1 : 0;
             }
         }
-        s += br[9];
-        if (s > 0) {
-            System.out.println("This number is 0");
-        } else {
-            System.out.println("This number is 1");
+        return neuron;
+    }
+
+    public static int getWeightedSum(int[] neuron) {
+        int max = 0;
+        int[] bias = {-1, 6, 1, 0, 2, 0, -1, 3, -2, -1};
+        int temp = 0;
+        int number = 0;
+        int[][] weights = {
+                {1, 1, 1, 1, -1, 1, 1, -1, 1, 1, -1, 1, 1, 1, 1},			//0
+                {-1, 1, -1, -1, 1, -1, -1, 1, -1, -1, 1, -1, -1, 1, -1},	//1
+                {1, 1, 1, -1, -1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1},			//2
+                {1, 1, 1, -1, -1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1},			//3
+                {1, -1, 1, 1, -1, 1, 1, 1, 1, -1, -1, 1, -1, -1, 1},		//4
+                {1, 1, 1, 1, -1, -1, 1, 1, 1, -1, -1, 1, 1, 1, 1},			//5
+                {1, 1, 1, 1, -1, -1, 1, 1, 1, 1, -1, 1, 1, 1, 1},			//6
+                {1, 1, 1, -1, -1, 1, -1, -1, 1, -1, -1, 1, -1, -1, 1},		//7
+                {1, 1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1},			//8
+                {1, 1, 1, 1, -1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1}			//9
+        };
+
+        for (int j = 0; j < 10; j++) {
+            for (int k = 0; k < 15; k++) {
+                temp += weights[j][k] * neuron[k];
+            }
+            temp += bias[j];
+            if (temp > max) {
+                max = temp;
+                number = j;
+            }
+            temp = 0;
         }
+        return number;
+    }
+
+    public static void main(String[] args) {
+        int[] neuron = getNeuron();
+        int result = getWeightedSum(neuron);
+        System.out.println(result);
+
     }
 }
